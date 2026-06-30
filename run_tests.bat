2de66a76e60fbb0e438e88c0713401f1d73740e9
Prathyu9100 <k.prathyusha005@gmail.com>
2026-07-01 01:04:37 +0530

    removed reports folder

--- run_tests.bat
+++ run_tests.bat
@@ -1,11 +1,6 @@
 @echo off
-
 cd /d "%~dp0"
 
-call .venv\Scripts\activate
-
-pytest --browser chrome --alluredir=reports
-
+python -m pip install -r requirements.txt
+python -m pytest --browser chrome --alluredir=reports
 allure generate reports --clean -o allure-report
-
-pause
